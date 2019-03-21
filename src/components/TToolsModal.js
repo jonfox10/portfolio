1cadf96d7f0f5054eb5137152c4934722f5c3e90
Jon Fox <jonfuchs45@gmail.com>
2019-03-20 18:06:47 -0600

    fixed a couple of bugs

--- src/components/TToolsModal.js
+++ src/components/TToolsModal.js
@@ -35,34 +35,23 @@ const TToolsModal = (props) => {
                 <h6 className='close-modal' onClick={() => props.setVisableTt(!props.visableTt)}>x</h6>
                 <div className='images'>
                     <div className='cynSlides'>
-                        {/* <div className='slideNumber'> 1 / 9 </div> */}
                             <video id='wideimg' className='modal-vid' controls autoplay loop src={ttImage1}>
-                                {/* <source src={ttImage1} type='video/mp4'/> */}
                             </video>
                     </div>
                     <div className='cynSlides'>
-                        {/* <div className='slideNumber'> 2 / 9 </div> */}
                             <img id='wideimg' className='modal-img'src={ttImage2}/>
                     </div>
                     <div className='cynSlides'>
-                        {/* <div className='slideNumber'> 3 / 9 </div> */}
                             <img id='wideimg' className='modal-img'src={ttImage3}/>
                     </div>
+                    
                     <div className='cynSlides'>
-                        {/* <div className='slideNumber'> 4 / 9 </div> */}
-                            <img id='wideimg' className='modal-img'src={ttImage4}/>
-                    </div>
-                    <div className='cynSlides'>
-                        {/* <div className='slideNumber'> 5 / 9 </div> */}
                             <img  className='modal-img'src={ttImage5} style={{borderRadius: '5vh'}}/>
                     </div>
                     <div className='cynSlides'>
-                        {/* <div className='slideNumber'> 6 / 9 </div> */}
                             <img  className='modal-img'src={ttImage6} style={{borderRadius: '3.5vh'}}/>
                     </div>
                 </div>    
-                {/* <button><img src={rightArrow} onClick={() => setSlide(slide + 1)}/></button>
-                <button><img onClick={() => setSlide(slide - 1)} src={leftArrow} /></button> */}
             </div>
 
             ) : (
